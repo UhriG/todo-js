45a2cf8bb3c59854d871fed38dd57b24ec7df95a
Cristian Uhrig <cristianuhrig@gmail.com>
2022-02-10 21:44:24 -0300

    inicializacion webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { saludar } from './js/componentes';
+import './styles.css';
+
+const nombre = 'Cristian Uhrig';
+
+saludar(nombre);
---
+++ webpack.prod.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+	mode: 'production',
+	optimization: {
+		minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+		minimize: true,
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, '/dist'),
+			watch: true,
+		},
+		historyApiFallback: true,
+		open: true,
+		compress: true,
+		hot: true,
+		port: 8080,
+		liveReload: true,
+		watchFiles: ['src/*.html'],
+	},
+	entry: './src/index.js',
+	output: {
+		filename: 'main.[contenthash].js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	devtool: false,
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+				exclude: /styles\.css$/,
+			},
+			{
+				test: /styles\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+			{
+				test: /\.html$/i,
+				loader: 'html-loader',
+				options: {
+					// Disables attributes processing
+					sources: false,
+				},
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							esModule: false,
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: path.resolve(__dirname, './src/index.html'),
+			filename: 'index.html',
+			inject: 'body',
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'styles/[name].[contenthash].css',
+			ignoreOrder: false,
+		}),
+		new CopyPlugin({
+			patterns: [{ from: 'src/assets', to: 'assets/' }],
+		}),
+	],
+};
