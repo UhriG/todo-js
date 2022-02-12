43859c428d4e27d25d1bf81291b74dc4a4745c95
Cristian Uhrig <cristianuhrig@gmail.com>
2022-02-12 11:55:48 -0300

    docs creado

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -4,7 +4,7 @@ export const saludar = (nombre) => {
 	console.log('Creando etiqueta h1 ');
 
 	const h1 = document.createElement('h1');
-	h1.innerHTML = `Hola ${nombre} al fin anda el live reload`;
+	h1.innerHTML = `Hola ${nombre}!`;
 
 	document.body.append(h1);
 };
